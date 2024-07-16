04dba9d5f066cfa35376076ce1cc0e457c87122b
guptashelly115 <41501385+guptashelly115@users.noreply.github.com>
2024-07-16 16:09:31 -0400

    Update losses.py
    Added other loss functions from other repositories that might be helpful.

--- losses.py
+++ losses.py
@@ -12,7 +12,7 @@ def focal_R_loss(inputs, targets, gamma = 5):
 def exponentially_weighted_mse_loss(inputs, targets):
     # This is my implementation for custom weighted loss. Here, labels 0 or closer to zero are abundant and are penalized more as compared to rarer outcomes that are further away from 0 and closer to 1.
     loss = F.mse_loss(inputs, targets, reduce=False).squeeze()
-    weights = torch.tensor([math.exp(math.exp(t)) for t in targets]).T
+    weights = torch.tensor([math.exp(t) for t in targets]).T
     loss *= weights
     loss = torch.mean(loss)
     return loss
@@ -24,4 +24,58 @@ def shrinkage_loss(inputs, targets, a = 12, c = 0.1):
     denominator = 1 + torch.exp(a * (c - l))
     loss = numerator / denominator
     loss = torch.mean(loss)
+    return loss
+
+################################################################################################################################################
+# Following code is from the mentioned github repository - https://github.com/YyzHarry/imbalanced-regression/blob/main/tutorial/tutorial.ipynb #
+################################################################################################################################################
+
+def weighted_mse_loss(inputs, targets, weights=None):
+    loss = (inputs - targets) ** 2
+    if weights is not None:
+        loss *= weights.expand_as(loss)
+    loss = torch.mean(loss)
+    return loss
+
+def weighted_l1_loss(inputs, targets, weights=None):
+    loss = F.l1_loss(inputs, targets, reduction='none')
+    if weights is not None:
+        loss *= weights.expand_as(loss)
+    loss = torch.mean(loss)
+    return loss
+
+def weighted_focal_mse_loss(inputs, targets, weights=None, activate='sigmoid', beta=.2, gamma=1):
+    loss = (inputs - targets) ** 2
+    loss *= (torch.tanh(beta * torch.abs(inputs - targets))) ** gamma if activate == 'tanh' else \
+        (2 * torch.sigmoid(beta * torch.abs(inputs - targets)) - 1) ** gamma
+    if weights is not None:
+        loss *= weights.expand_as(loss)
+    loss = torch.mean(loss)
+    return loss
+
+def weighted_focal_l1_loss(inputs, targets, weights=None, activate='sigmoid', beta=.2, gamma=1):
+    loss = F.l1_loss(inputs, targets, reduction='none')
+    loss *= (torch.tanh(beta * torch.abs(inputs - targets))) ** gamma if activate == 'tanh' else \
+        (2 * torch.sigmoid(beta * torch.abs(inputs - targets)) - 1) ** gamma
+    if weights is not None:
+        loss *= weights.expand_as(loss)
+    loss = torch.mean(loss)
+    return loss
+
+def weighted_huber_loss(inputs, targets, weights=None, beta=1.):
+    l1_loss = torch.abs(inputs - targets)
+    cond = l1_loss < beta
+    loss = torch.where(cond, 0.5 * l1_loss ** 2 / beta, l1_loss - 0.5 * beta)
+    if weights is not None:
+        loss *= weights.expand_as(loss)
+    loss = torch.mean(loss)
+    return loss
+
+def bmse_loss(inputs, targets, noise_sigma=8.):
+    return bmc_loss(inputs, targets, noise_sigma ** 2)
+
+def bmc_loss(pred, target, noise_var):
+    logits = - 0.5 * (pred - target.T).pow(2) / noise_var
+    loss = F.cross_entropy(logits, torch.arange(pred.shape[0]))
+    loss = loss * (2 * noise_var)
     return loss
